c05ba8d4c0841c412b1bdf87e31fe66b841cecad
Giuseppe Grilli <giugrilli@gmail.com>
2017-01-12 03:03:15 +0100

    es6 advanced as default

--- lib/closure/compiler.rb
+++ lib/closure/compiler.rb
@@ -14,7 +14,8 @@ module Closure
 
     DEFAULT_OPTIONS = {
       :warning_level => 'QUIET',
-      :language_in => 'ECMASCRIPT5'
+      :language_in => 'ECMASCRIPT6',
+      :compilation_level => 'ADVANCED_OPTIMIZATIONS'
     }
 
     # When you create a Compiler, pass in the flags and options.
